2d841851a960efe282e30292e44810b12d5b2f08
Maegan Wilson <maegan@maeganwilson.com>
2022-03-18 08:29:37 -0500

    feat: introduces a new view

---
+++ CoffeeTracker/CoffeeTracker/About/AboutView.swift
@@ -0,0 +1,20 @@
+//
+//  AboutView.swift
+//  CoffeeTracker
+//
+//  Created by Maegan Wilson on 3/18/22.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text("About")
+    }
+}
+
+struct AboutView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutView()
+    }
+}

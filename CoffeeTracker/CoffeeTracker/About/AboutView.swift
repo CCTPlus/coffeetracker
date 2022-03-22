a4ef77e1fa56a18379acbfcd5e5f8fdce6e59102
Maegan Wilson <maegan@maeganwilson.com>
2022-03-22 16:54:57 -0500

    fix: adds way to access the share sheet to about

--- CoffeeTracker/CoffeeTracker/About/AboutView.swift
+++ CoffeeTracker/CoffeeTracker/About/AboutView.swift
@@ -8,10 +8,45 @@
 import SwiftUI
 
 struct AboutView: View {
+
+    @Environment(\.colorScheme) private var colorScheme
+    @State private var showingSheet = false
+
     var body: some View {
-        VStack {
-            BuyDevCoffeeSection()
-        }.padding()
+        ZStack {
+            Image("Background")
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                HStack {
+                    Spacer()
+                        .frame(height: 80)
+                }
+                BuyDevCoffeeSection()
+                    .padding(.bottom, 10)
+                ScrollView {
+                    ShareApp()
+                        .padding(.vertical, 10)
+                }
+            }.padding()
+            Group {
+                GeometryReader { geometry in
+                    HStack(alignment: .bottom) {
+                        Text("About")
+                            .font(.largeTitle)
+                            .bold()
+                            .padding(.top, 10)
+                            .padding(.leading)
+                        Spacer()
+                    }
+                    .frame(height: geometry.safeAreaInsets.top+80)
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(10, corners: .bottomLeft)
+                    .cornerRadius(10, corners: .bottomRight)
+                    .edgesIgnoringSafeArea(.top)
+                }
+            }.shadow(radius: 2)
+        }
     }
 }
 

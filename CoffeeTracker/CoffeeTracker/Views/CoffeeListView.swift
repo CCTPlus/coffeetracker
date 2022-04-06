972901d842170ed385978800e09e739b0de447cc
Maegan Wilson <maegan@maeganwilson.com>
2022-04-05 22:19:10 -0500

    Adds filter picker

--- CoffeeTracker/CoffeeTracker/Views/CoffeeListView.swift
+++ CoffeeTracker/CoffeeTracker/Views/CoffeeListView.swift
@@ -14,6 +14,9 @@ struct CoffeeListView: View {
 
     @State private var search: String = ""
 
+    @State private var beanTypeFilter = "all"
+    let beanTypes = ["all", "pods", "beans", "grounds"]
+
     var body: some View {
         ZStack(alignment: .top) {
             ScrollView {
@@ -21,10 +24,18 @@ struct CoffeeListView: View {
                     Spacer()
                         .frame(height: Design.base*10)
                 }
-                ForEach(beansOO.beans, id: \.self) { bean in
-                    BeanRowView(bean: bean)
-                        .padding(.vertical, Design.base/2)
-                        .padding(.horizontal)
+                if beanTypeFilter == "all" {
+                    ForEach(beansOO.beans, id: \.self) { bean in
+                        BeanRowView(bean: bean)
+                            .padding(.vertical, Design.base/2)
+                            .padding(.horizontal)
+                    }
+                } else {
+                    ForEach(beansOO.beans.filter { $0.beanType == beanTypeFilter }, id: \.self) { bean in
+                        BeanRowView(bean: bean)
+                            .padding(.vertical, Design.base/2)
+                            .padding(.horizontal)
+                    }
                 }
                 HStack {
                     Spacer()
@@ -33,15 +44,22 @@ struct CoffeeListView: View {
             }
             Group {
                 GeometryReader { geometry in
-                    HStack(alignment: .bottom) {
-                        Text("Coffee")
-                            .font(.largeTitle)
-                            .bold()
-                            .padding(.top, Design.base)
-                            .padding(.leading)
-                        Spacer()
+                    VStack(spacing: 0) {
+                        HStack(alignment: .bottom) {
+                            Text("Coffee")
+                                .font(.largeTitle)
+                                .bold()
+                                .padding(.leading)
+                            Spacer().padding(.top, Design.base*8)
+                        }
+                        Picker("Bean Type", selection: $beanTypeFilter) {
+                            ForEach(beanTypes, id: \.self) { type in
+                                Text(type.capitalized)
+                            }
+                        }.pickerStyle(.segmented)
+                            .padding(.all, Design.base*2)
                     }
-                    .frame(height: geometry.safeAreaInsets.top+Design.base*8)
+                    .frame(height: geometry.safeAreaInsets.top+Design.base*10)
                     .background(.ultraThinMaterial)
                     .cornerRadius(Design.base, corners: .bottomLeft)
                     .cornerRadius(Design.base, corners: .bottomRight)

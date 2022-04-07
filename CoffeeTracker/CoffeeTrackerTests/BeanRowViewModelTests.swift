05c1edb7f87c0bd27afba6697adcd8f241ad6c82
Maegan Wilson <maegan@maeganwilson.com>
2022-04-06 22:55:50 -0500

    Adding tests

---
+++ CoffeeTracker/CoffeeTrackerTests/BeanRowViewModelTests.swift
@@ -0,0 +1,53 @@
+//
+//  CoffeeTrackerTests.swift
+//  CoffeeTrackerTests
+//
+//  Created by Maegan Wilson on 4/6/22.
+//
+
+import XCTest
+import SwiftUI
+@testable import Coffee_
+
+class CoffeeTrackerTests: XCTestCase {
+
+    var sut: BeanRowViewModel?
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        sut = BeanRowViewModel(bean: testRoast)
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete.
+        // Check the results with assertions afterwards.
+    }
+
+    func test_toggleDetails() {
+        guard let sut = sut else {
+            return
+        }
+        XCTAssertFalse(sut.showDetails)
+        sut.toggleDetails()
+        XCTAssert(sut.showDetails)
+    }
+
+    func test_coffeTypeIcon() {
+        guard let sut = sut else {
+            return
+        }
+
+        XCTAssertTrue(sut.coffeeTypeIcon == Image(systemName: SFSymbols.rectRoundedBottom))
+    }
+
+}

1ef91ac668d801cc4f9ebe4bc9c227e0cca5f6ad
Maegan Wilson <maegan@maeganwilson.com>
2022-04-06 21:50:21 -0500

    Viewmodel for beanrow added

--- CoffeeTracker/CoffeeTracker/Views/BeanRow/BeanRowView.swift
+++ CoffeeTracker/CoffeeTracker/Views/BeanRow/BeanRowView.swift
@@ -11,15 +11,13 @@ import SwiftUI
 struct BeanRowView: View {
     @Environment(\.colorScheme) private var colorScheme
 
-    @State private var showingDetails = false
-
-    var bean: BeanModel
+    @StateObject var viewModel: BeanRowViewModel
 
     var tap: some Gesture {
         TapGesture(count: 1)
             .onEnded { _ in
                 withAnimation {
-                    showingDetails.toggle()
+                    viewModel.toggleDetails()
                 }
             }
     }
@@ -27,7 +25,7 @@ struct BeanRowView: View {
     var body: some View {
         VStack {
             HStack(alignment: .top, spacing: Design.base*2) {
-                Image(uiImage: bean.image)
+                Image(uiImage: viewModel.image)
                     .resizable()
                     .scaledToFit()
                     .cornerRadius(Design.base)
@@ -35,54 +33,39 @@ struct BeanRowView: View {
                 VStack(alignment: .leading, spacing: Design.base/2) {
                     HStack(alignment: .top) {
                         VStack(alignment: .leading) {
-                            Text(bean.name)
+                            Text(viewModel.name)
                                 .font(.headline)
-                            Text(bean.roaster)
+                            Text(viewModel.roaster)
                                 .font(.subheadline)
                                 .padding(.bottom, Design.base)
                         }
                         Spacer()
-                        if bean.buyAgain {
+                        if viewModel.buyAgain {
                             Image(systemName: SFSymbols.thumbsup)
                                 .opacity(0.60)
                         }
                     }
                     HStack(alignment: .bottom) {
-                        Text(bean.style)
+                        Text(viewModel.style)
                             .font(.footnote)
                         Spacer()
-                        switch bean.beanType {
-                        case "pods":
-                            Image(systemName: SFSymbols.rectRoundedBottom)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 20, height: 20, alignment: .center)
-                                .opacity(0.5)
-                        case "grounds":
-                            Image("CoffeeBag")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 20, height: 20, alignment: .center)
-                                .opacity(0.5)
-                        default:
-                            Image("CoffeeBean")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 20, height: 20, alignment: .center)
-                                .opacity(0.5)
-                        }
+                        viewModel.coffeeTypeIcon
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20, alignment: .center)
+                            .opacity(0.5)
                     }
                 }
             }.padding(.horizontal, Design.base).padding(.top, Design.base)
-                .padding(.bottom, !showingDetails ? Design.base : 0)
-            if showingDetails {
-                BeanDetailView(navRouter: NavigationRouter(), bean: bean)
+                .padding(.bottom, viewModel.bottomPadding)
+
+            if viewModel.showDetails {
+                BeanDetailView(navRouter: NavigationRouter(), bean: viewModel.bean)
             }
 
         }.frame(maxWidth: .infinity)
             .row(padding: false)
             .gesture(tap)
-//            .animation(.default, value: showingDetails)
     }
 }
 
@@ -93,10 +76,10 @@ struct BeanRowView_Previews: PreviewProvider {
                 .resizable()
                 .ignoresSafeArea()
             ScrollView {
-                BeanRowView(bean: mediumRoast)
+                BeanRowView(viewModel: BeanRowViewModel(bean: testRoast))
                     .padding(.vertical, Design.base/2)
                     .padding(.horizontal)
-                BeanRowView(bean: testRoast)
+                BeanRowView(viewModel: BeanRowViewModel(bean: testRoast))
                     .padding(.vertical, Design.base/2)
                     .padding(.horizontal)
             }
@@ -107,10 +90,10 @@ struct BeanRowView_Previews: PreviewProvider {
                 .resizable()
                 .ignoresSafeArea()
             ScrollView {
-                BeanRowView(bean: testRoast)
+                BeanRowView(viewModel: BeanRowViewModel(bean: testRoast))
                     .padding(.vertical, Design.base/2)
                     .padding(.horizontal)
-                BeanRowView(bean: testRoast)
+                BeanRowView(viewModel: BeanRowViewModel(bean: testRoast))
                     .padding(.vertical, Design.base/2)
                     .padding(.horizontal)
             }
---
+++ CoffeeTracker/CoffeeTracker/Views/BeanRow/BeanRowViewModel.swift
@@ -0,0 +1,60 @@
+//
+//  BeanRowViewModel.swift
+//  CoffeeTracker
+//
+//  Created by Maegan Wilson on 4/6/22.
+//
+
+import SwiftUI
+
+final class BeanRowViewModel: ObservableObject {
+    @Published var showDetails = false
+
+    let bean: BeanModel
+
+    var bottomPadding: CGFloat {
+        if !showDetails {
+            return Design.base
+        } else {
+            return 0
+        }
+    }
+
+    var coffeeTypeIcon: Image {
+        switch bean.beanType {
+        case "pods":
+            return Image(systemName: SFSymbols.rectRoundedBottom)
+        case "grounds":
+            return Image("CoffeeBag")
+        default:
+            return Image("CoffeeBean")
+        }
+    }
+
+    var image: UIImage {
+        bean.image
+    }
+
+    var name: String {
+        bean.name
+    }
+
+    var roaster: String {
+        bean.roaster
+    }
+
+    var buyAgain: Bool {
+        bean.buyAgain
+    }
+
+    var style: String {
+        bean.style
+    }
+    init(bean: BeanModel) {
+        self.bean = bean
+    }
+
+    func toggleDetails() {
+        showDetails.toggle()
+    }
+}

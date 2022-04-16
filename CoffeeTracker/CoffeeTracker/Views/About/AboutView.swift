b30ba67218180670665f805db78cf6b2b2d3dda7
Maegan Wilson <maegan@maeganwilson.com>
2022-04-16 16:01:11 -0500

    Changes about navigation view

--- CoffeeTracker/CoffeeTracker/Views/About/AboutView.swift
+++ CoffeeTracker/CoffeeTracker/Views/About/AboutView.swift
@@ -15,72 +15,55 @@ struct AboutView: View {
     @State private var showingWhatsNew = false
 
     var body: some View {
-        ZStack {
-            Image("Background")
-                .resizable()
-                .ignoresSafeArea()
-            VStack {
+        ScrollView {
+            BuyDevCoffeeSection().padding()
+            ShareApp().padding()
+            Button {
+                showFeedback = true
+            } label: {
+                HStack(spacing: 15) {
+                    Image(systemName: SFSymbols.plane)
+                        .foregroundColor(.pink)
+                        .font(.system(size: 36))
+                    Text("Send feedback")
+                        .foregroundColor(.primary)
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                    .row().padding()
+
+            }.sheet(isPresented: $showFeedback) {
+                UserFeedbackView()
+            }
+            Button {
+                showingWhatsNew = true
+            } label: {
+                HStack(spacing: 15) {
+                    Image(systemName: SFSymbols.star)
+                        .foregroundColor(.pink)
+                        .font(.system(size: 36))
+                    Text("Release Notes")
+                        .foregroundColor(.primary)
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                    .row().padding()
+
+            }.sheet(isPresented: $showingWhatsNew) {
+                WhatsNewView()
+            }
+
+            HStack {
+                Spacer()
+                    .frame(height: 120)
+            }
+        }.safeAreaInset(edge: .top) {
+            VStack(alignment: .center, spacing: 8) {
                 HStack {
+                    Text("About")
+                        .font(.largeTitle.weight(.bold))
                     Spacer()
-                        .frame(height: 70)
-                }
-                ScrollView {
-                    BuyDevCoffeeSection().padding()
-                    ShareApp().padding()
-                    Button {
-                        showFeedback = true
-                    } label: {
-                        HStack(spacing: 15) {
-                            Image(systemName: SFSymbols.plane)
-                                .foregroundColor(.pink)
-                                .font(.system(size: 36))
-                            Text("Send feedback")
-                                .foregroundColor(.primary)
-                        }.frame(maxWidth: .infinity, alignment: .leading)
-                            .row().padding()
-
-                    }.sheet(isPresented: $showFeedback) {
-                        UserFeedbackView()
-                    }
-                    Button {
-                        showingWhatsNew = true
-                    } label: {
-                        HStack(spacing: 15) {
-                            Image(systemName: SFSymbols.star)
-                                .foregroundColor(.pink)
-                                .font(.system(size: 36))
-                            Text("Release Notes")
-                                .foregroundColor(.primary)
-                        }.frame(maxWidth: .infinity, alignment: .leading)
-                            .row().padding()
-
-                    }.sheet(isPresented: $showingWhatsNew) {
-                        WhatsNewView()
-                    }
-
-                    HStack {
-                        Spacer()
-                            .frame(height: 120)
-                    }
                 }
             }
-            Group {
-                GeometryReader { geometry in
-                    HStack(alignment: .bottom) {
-                        Text("About")
-                            .font(.largeTitle)
-                            .bold()
-                            .padding(.top, 10)
-                            .padding(.leading)
-                        Spacer()
-                    }
-                    .background(.ultraThinMaterial)
-                    .cornerRadius(10, corners: .bottomLeft)
-                    .cornerRadius(10, corners: .bottomRight)
-                    .edgesIgnoringSafeArea(.top)
-                }
-            }.shadow(radius: 2)
-        }
+            .padding()
+            .background(.ultraThinMaterial)
+        }.navigationBarHidden(true)
     }
 }
 

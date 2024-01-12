cbf657dd2e98b2da6ef954868512ce66c6d16429
Jay <heyjay@cctplus.dev>
2024-01-12 11:20:06 -0600

    feat: form for new bean
    creates a model for the bean
    close: #68

---
+++ App/AppCore/Sources/Beans/NewBeanView.swift
@@ -0,0 +1,53 @@
+//
+//  NewBeanView.swift
+//
+//
+//  Created by Jay on 1/12/24.
+//
+
+import SwiftUI
+import Models
+
+struct NewBeanView: View {
+    var save: () -> Void
+    var cancel: () -> Void
+
+    @State private var newBean = Bean(
+        name: "",
+        website: "",
+        roastStyle: .medium
+    )
+
+    var body: some View {
+        Form {
+            TextField("Name", text: $newBean.name)
+            TextField("Website", text: $newBean.website)
+            Picker("Roast Style", selection: $newBean.roastStyle) {
+                ForEach(RoastStyle.allCases) { roastStyle in
+                    Text(roastStyle.label)
+                        .tag(roastStyle)
+                }
+            }
+        }
+        .navigationTitle("New Bean")
+        .navigationBarTitleDisplayMode(.large)
+        .toolbar {
+            ToolbarItem {
+                Button("Save") {
+                    save()
+                }
+            }
+            ToolbarItem(placement: .topBarLeading) {
+                Button("Cancel") {
+                    cancel()
+                }.tint(.red)
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        NewBeanView(save: { print("save") }, cancel: { print("cancel") })
+    }
+}
---
+++ App/AppCore/Sources/Models/Bean.swift
@@ -0,0 +1,42 @@
+//
+//  Bean.swift
+//
+//
+//  Created by Jay on 1/12/24.
+//
+
+import Foundation
+
+public enum RoastStyle: Int, Identifiable, CaseIterable {
+    public var id: Int {
+        self.rawValue
+    }
+
+    case light, medium, dark
+
+    public var label: String {
+        switch self {
+            case .light:
+                "Light roast"
+            case .medium:
+                "Medium roast"
+            case .dark:
+                "Dark roast"
+        }
+    }
+}
+
+public struct Bean: Equatable {
+    public let id: UUID
+
+    public var name: String
+    public var website: String
+    public var roastStyle: RoastStyle
+
+    public init(id: UUID = UUID(), name: String, website: String, roastStyle: RoastStyle) {
+        self.id = id
+        self.name = name
+        self.website = website
+        self.roastStyle = roastStyle
+    }
+}
